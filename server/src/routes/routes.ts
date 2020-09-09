3b5da2de2c6f6bc18f3126326e949adaba3c4f1d
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-09 18:47:34 -0300

    feat: add some routes to project

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,10 +1,21 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface IRequestBody extends Request{
     body: { [key: string]: string | undefined };
 }
 
 const routes = Router();
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send(`
+        <div>Not permitted</div>
+        <a href="/login">Please login to acess this page!</a>
+    `);
+}
 
 routes.get("/login", (req: Request, res: Response) => {
     res.send(`
@@ -29,5 +40,31 @@ routes.post("/login", (req: IRequestBody, res: Response) => {
     } else {
         res.send("You must provide an email");
     }
+});
+
+routes.get("/", (req: IRequestBody, res: Response) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+        <div>
+            <div>You are logged in</div>
+            <a href="/logout">Logout</a>
+        </div>
+        `)
+    } else {
+        res.send(`
+        <div>
+            <div>You are not logged</div>
+            <a href="/login">login</a>
+        </div>
+        `);
+    }
+});
+routes.get("/logout", (req: Request, res: Response) => {
+    req.session = null;
+    res.redirect("/");
+
+});
+routes.get("/protected", requireAuth, (req: Request, res: Response) => {
+    res.send(`Welcome to protected route, logged in user`);
 });
 export default routes;
