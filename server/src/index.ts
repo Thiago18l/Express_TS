500e096d9d478234d434b1120120923b59424ec3
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-10 20:06:25 -0300

    feat: add AppRouter to make instances of express

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import Routes from "./routes/routes";
 import bodyParser from 'body-parser';
 import cookiesSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookiesSession({ keys: ['value']}))
 app.use(Routes);
+app.use(AppRouter.getInstance());
 
 app.listen(PORT, () => {
     console.log(`Running in the port ${PORT}`);
